3582107109f8216f21f1dfff3204fdac67186fa9
ines-vermeir <ines.vermeir@student.ehb.be>
2018-03-28 15:54:34 +0200

    app files
    app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FeedbackPage } from '../pages/feedback/feedback';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FeedbackPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FeedbackPage
   ],
   providers: [
     StatusBar,
